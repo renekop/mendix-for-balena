cae769e76fb69a9c89908090af3fbf48153f07b7
René Kop <rene.kop@linkit.nl>
2024-10-15 16:01:51 +0200

    incl. mx model

---
+++ docker-mendix-buildpack/project/native/styles.js
@@ -0,0 +1,11 @@
+import * as style0 from "C:/Users/rene.kop/AppData/Local/Temp/gakgxewu.2bb/themesource/administration/native/main";
+import * as style1 from "C:/Users/rene.kop/AppData/Local/Temp/gakgxewu.2bb/themesource/nanoflowcommons/native/main";
+import * as style2 from "C:/Users/rene.kop/AppData/Local/Temp/gakgxewu.2bb/themesource/webactions/native/main";
+import * as style3 from "C:/Users/rene.kop/AppData/Local/Temp/gakgxewu.2bb/themesource/atlas_core/native/main";
+import * as style4 from "C:/Users/rene.kop/AppData/Local/Temp/gakgxewu.2bb/themesource/feedbackmodule/native/main";
+import * as style5 from "C:/Users/rene.kop/AppData/Local/Temp/gakgxewu.2bb/themesource/myfirstmodule/native/main";
+import * as style6 from "C:/Users/rene.kop/AppData/Local/Temp/gakgxewu.2bb/theme/native/main";
+
+import { flatten } from "mendix/native";
+
+module.exports = flatten([style0, style1, style2, style3, style4, style5, style6]);
---
+++ docker-mendix-buildpack/project/react-web/start.ps1
@@ -0,0 +1,1 @@
+C:/Program Files/Mendix/9.24.18.31862\modeler\tools\node\win-x64\node.exe C:/Program Files/Mendix/9.24.18.31862\modeler\tools\node\node_modules\rollup\dist\bin\rollup -c -w
